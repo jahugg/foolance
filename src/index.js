497d0069ff3059213c71975d10e8012724dd6a82
Jan <jan@some-new-macbook-pro-1.home>
2020-11-12 08:52:02 +0100

    mongo db connection tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hi");
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbUser:oPia3I2Ok1R5jlnB@cluster0.beqox.mongodb.net/Cluster0?retryWrites=true&w=majority";
+connect();
+async function connect() {
+    const cline = new MongoClient(uri);
+    try {
+        await client.connect();
+        const db = client.db(`cluster0`);
+        console.log(`Connected to database ${db.databaseName}`);
+    }
+    catch (ex) {
+        console.error(`Exception: ${ex}`);
+    }
+    finally {
+        client.close();
+    }
+}
